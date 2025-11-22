f2b4b97cdd49bd0eb0d591e387dfb9f27b49738c
Amfrd_luci <gsraghul2003@gmail.com>
2025-11-22 18:29:44 +0530

    Update todo.js

--- server/todo.js
+++ server/todo.js
@@ -1,55 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-const fs = require('fs');
+import fs from "fs";
+import path from "path";
 
-const app = express();
-const PORT = 4000;
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), "todo.json");
 
-app.use(cors());
-app.use(express.json());
+  // Read data
+  const data = JSON.parse(fs.readFileSync(filePath, "utf-8"));
 
-// Helper function to read todos
-function readTodos() {
-  const data = fs.readFileSync('./todo.json', 'utf-8');
-  return JSON.parse(data);
+  if (req.method === "GET") {
+    return res.status(200).json(data.todos);
+  }
+
+  if (req.method === "POST") {
+    const { item } = req.body;
+    data.todos.push(item);
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+    return res.status(200).json({ message: "Todo added", todos: data.todos });
+  }
+
+  if (req.method === "DELETE") {
+    const { item } = req.body;
+    data.todos = data.todos.filter(t => t !== item);
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+    return res.status(200).json({ message: "Todo deleted", todos: data.todos });
+  }
+
+  res.status(405).send("Method Not Allowed");
 }
-
-// Helper function to write todos
-function writeTodos(data) {
-  fs.writeFileSync('./todo.json', JSON.stringify(data, null, 2));
-}
-
-// ROUTES
-
-// Get all todos
-app.get('/todos', (req, res) => {
-  const data = readTodos();
-  res.json(data.todos);
-});
-
-// Add new todo
-app.post('/add', (req, res) => {
-  const { item } = req.body;
-  const data = readTodos();
-
-  data.todos.push(item);
-  writeTodos(data);
-
-  res.json({ message: "Todo added", todos: data.todos });
-});
-
-// Delete todo
-app.delete('/delete', (req, res) => {
-  const { item } = req.body;
-  const data = readTodos();
-
-  data.todos = data.todos.filter(t => t !== item);
-  writeTodos(data);
-
-  res.json({ message: "Todo deleted", todos: data.todos });
-});
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
